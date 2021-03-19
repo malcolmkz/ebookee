9017f21d40fcb5704731123f5e3071e9d4c0100a
Malek KAZDZAGHLI <malek.kazdaghli@sofrecom.com>
2021-03-19 15:53:32 +0100

    Updated config.ts with aws amplify callback url

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,7 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-0mk8tvm7.eu.auth0.com',            // Auth0 domain
   clientId: '01SqUCmZNbHPac5uaatgqofGRMe2L7cu',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  //callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://main.d380wxkxnfivi1.amplifyapp.com/callback '
+  
 }
